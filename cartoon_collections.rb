1c224f9f0cfae4b2355fa6819b7f8f407bf225ba
Santiago Sendon <santiagosendon92@gmail.com>
2018-08-19 21:10:41 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,35 +1,28 @@
-def roll_call_dwarves(dwarfs)# code an argument here
-  # Your code here
-  #dwarfs = ["Doc", "Dopey", "Bashful", "Grumpy"]
-  dwarfs.each_with_index do | name , index |
-    puts "#{index + 1} #{name}"
+def roll_call_dwarves(dwarves)
+  dwarves.each_with_index do |name, index|
+    index += 1
+    puts "#{index}. #{name}"
   end
 end
 
-def summon_captain_planet(veggies)# code an argument here
-  # Your code here
-  # planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
-    veggies.collect do |call| 
-    call.capitalize + "!"
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.collect do |call|
+    call.capitalize << "!"
   end
-
 end
 
-def long_planteer_calls(long_planteer_calls)# code an argument here
-  # Your code here
-  answer = false
-  long_planteer_calls.each do |call|
-    if call.length > 4
-      answer = true
-    end
+def long_planeteer_calls(calls)
+  calls.any? do |call|
+    call.length > 4
   end
-    answer
 end
 
-def find_the_cheese(cheese)# code an argument here
+def find_the_cheese(array)
   # the array below is here to help
+  i = 0
   cheese_types = ["cheddar", "gouda", "camembert"]
-  cheese.find do |cheese|
-    cheese_types.include?(cheese)
+  while i < cheese_types.length
+    return cheese_types[i] if array.include?(cheese_types[i])
+    i += 1
   end
 end
