a8491456c8623726709004164d6347892a42f624
Santiago Sendon <santiagosendon92@gmail.com>
2018-08-19 21:09:25 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,4 @@
-def roll_call_dwarves(dwarfs)# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarfs)
   dwarfs = ["Doc", "Dopey", "Bashful", "Grumpy"]
   dwarfs.each_with_index do | name , index |
     puts "#{index + 1} #{name}"
