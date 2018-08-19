4ea196225e43d1f45f45eff31b7fcbd14852b79f
Santiago Sendon <santiagosendon92@gmail.com>
2018-08-19 21:07:03 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,6 @@
 def roll_call_dwarves(dwarfs)# code an argument here
   # Your code here
-  #dwarfs = ["Doc", "Dopey", "Bashful", "Grumpy"]
+  dwarfs = ["Doc", "Dopey", "Bashful", "Grumpy"]
   dwarfs.each_with_index do | name , index |
     puts "#{index + 1} #{name}"
   end
@@ -8,7 +8,7 @@ end
 
 def summon_captain_planet(veggies)# code an argument here
   # Your code here
-  # planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
+   planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
     veggies.collect do |call| 
     call.capitalize + "!"
   end
